af51649f9cc950f79ecd93915146ded6813e6a97
YusukeSano <yuu131129@gmail.com>
2023-06-09 01:44:28 +0900

    :sparkles: Add favicon

--- src/components/SEO/SEO.tsx
+++ src/components/SEO/SEO.tsx
@@ -17,6 +17,31 @@ export const SEO: React.FC<Props> = ({ children }) => {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="theme-color" content={themeColor} />
+      <meta name="msapplication-config" content="/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content={themeColor} />
+      <link rel="manifest" href="/site.webmanifest" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicons/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="/favicons/safari-pinned-tab.svg"
+        color={themeColor}
+      />
       {children}
     </>
   )
