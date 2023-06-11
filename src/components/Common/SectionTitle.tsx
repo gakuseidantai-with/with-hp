7b91d813ff6e20509ec9cf71f2ee144eb3aaa02a
misaki <swimswiftcode@gmail.com>
2023-06-11 14:43:06 +0900

    update section title

--- src/components/Common/SectionTitle.tsx
+++ src/components/Common/SectionTitle.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
 
 import * as styles from '@/components/Common/SectionTitle.module.scss'
+import iconPath from '@/images/logo/icon.svg'
 type Props = {
   title: String
 }
 
 export const SectionTitle: React.FC<Props> = ({ title }) => {
-  return <h2 className={styles['sectionTitle']}>{title}</h2>
+  return (
+    <>
+      <img
+        className={styles['icon']}
+        src={iconPath}
+        alt="学生団体withロゴマーク"
+      />
+      <h2 className={styles['sectionTitle']}>{title}</h2>
+    </>
+  )
 }
