6813f0aca8549cb032e080dddfca6839d758a1b2
YusukeSano <yuu131129@gmail.com>
2023-07-10 03:27:31 +0900

    :sparkles: Add ArrowTitle component

---
+++ src/components/common/ArrowTitle/ArrowTitle.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+import * as styles from '@/components/common/ArrowTitle/ArrowTitle.module.scss'
+
+type Props = {
+  children: string
+}
+
+export const ArrowTitle: React.FC<Props> = ({ children }) => {
+  return (
+    <h2 className={styles['title']}>
+      <span className={styles['text']}>{children}</span>
+      <span className={styles['arrow']} />
+    </h2>
+  )
+}
